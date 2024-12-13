9b554e98e2314382f4f0f8ee1927efe6a6421a39
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-12-13 11:28:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WEREWOLF-DEMON/NobimusicDemon",
+    "https://github.com/istkharalam62/ISTKHAR",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
