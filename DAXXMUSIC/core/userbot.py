581ed83473aa577e9d3c8f502b2dfb1ee6f912c7
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-12-15 07:35:57 +0530

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("tmm_heroku_world")
+                await self.one.join_chat("CRAZY_OP_02")
               
-                await self.one.join_chat("tmm_support_chat")
+                await self.one.join_chat("PURVI_SUPPORT")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("tmm_heroku_world")
+                await self.two.join_chat("CRAZY_OP_02")
                 
-                await self.one.join_chat("tmm_support_chat")
+                await self.one.join_chat("PURVI_SUPPORT")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("tmm_heroku_world")
+                await self.three.join_chat("CRAZY_OP_02")
                 
-                await self.one.join_chat("tmm_support_chat")
+                await self.one.join_chat("PURVI_SUPPORT")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("tmm_heroku_world")
+                await self.four.join_chat("CRAZY_OP_02")
                 
-                await self.one.join_chat("tmm_support_chat")
+                await self.one.join_chat("PURVI_SUPPORT")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("tmm_heroku_world")
+                await self.five.join_chat("CRAZY_OP_02")
                 
-                await self.one.join_chat("tmm_support_chat")
+                await self.one.join_chat("PURVI_SUPPORT")
             except:
                 pass
             assistants.append(5)
